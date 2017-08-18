33efea535de7f27aae2c2c9ac0b9b788b8e0a7cb
Priya Mangal <mangalpriya67@gmail.com>
2017-08-18 08:06:35 -0700

    Work done

--- W3D4/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ext/nokogiri/tmp/x86_64-apple-darwin16.6.0/ports/libxml2/2.9.4/libxml2-2.9.4/python/setup.py
+++ W3D4/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ext/nokogiri/tmp/x86_64-apple-darwin16.6.0/ports/libxml2/2.9.4/libxml2-2.9.4/python/setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup, Extension
 # Below ROOT, we expect to find include, include/libxml2, lib and bin.
 # On *nix, it is not needed (but should not harm),
 # on Windows, it is set by configure.js.
-ROOT = r'/Users/priyamangal/Downloads/W3D4_Hw/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ports/x86_64-apple-darwin16.6.0/libxml2/2.9.4'
+ROOT = r'/Users/priyamangal/Documents/homeworks/W3D4/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ports/x86_64-apple-darwin16.6.0/libxml2/2.9.4'
 
 # Thread-enabled libxml2
 with_threads = 1
--- W3D4/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ext/nokogiri/tmp/x86_64-apple-darwin16.6.0/ports/libxml2/2.9.4/libxml2-2.9.4/xml2Conf.sh
+++ W3D4/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ext/nokogiri/tmp/x86_64-apple-darwin16.6.0/ports/libxml2/2.9.4/libxml2-2.9.4/xml2Conf.sh
@@ -1,8 +1,8 @@
 #
 # Configuration file for using the XML library in GNOME applications
 #
-XML2_LIBDIR="-L/Users/priyamangal/Downloads/W3D4_Hw/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ports/x86_64-apple-darwin16.6.0/libxml2/2.9.4/lib"
+XML2_LIBDIR="-L/Users/priyamangal/Documents/homeworks/W3D4/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ports/x86_64-apple-darwin16.6.0/libxml2/2.9.4/lib"
 XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/priyamangal/Downloads/W3D4_Hw/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ports/x86_64-apple-darwin16.6.0/libxml2/2.9.4/include/libxml2"
+XML2_INCLUDEDIR="-I/Users/priyamangal/Documents/homeworks/W3D4/N+1 Buster/skeleton/vendor/bundle/gems/nokogiri-1.8.0/ports/x86_64-apple-darwin16.6.0/libxml2/2.9.4/include/libxml2"
 MODULE_VERSION="xml2-2.9.4"
 
