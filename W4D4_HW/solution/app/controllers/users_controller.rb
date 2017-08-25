06a76edeee8abe698030b5c9477b38937809f717
Priya Mangal <mangalpriya67@gmail.com>
2017-08-25 08:01:42 -0700

    w4d4 hw

---
+++ W4D4_HW/solution/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :require_no_user!
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user!(@user)
+
+      # u = User.create(user_params)
+      u = @user
+      msg = UserMailer.welcome_email(u)
+      msg.deliver_now
+
+      redirect_to @url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :username)
+  end
+end
